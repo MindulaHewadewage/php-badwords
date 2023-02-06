16e6fd94e989f95e1f10e94f55ccdfb5dbd8c7a8
MindulaHewadewage <mindufernando@gmail.com>
2023-02-06 22:58:41 +0100

    struttura php(da fixare)

--- badwords.php
+++ badwords.php
@@ -1,3 +1,10 @@
+<?php 
+$word = $_GET['word'];
+$text_area = $_GET['text_area'];
+$censored_word = str_replace($word,'***',$text_area);
+$text_length = strlen($text_area);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +14,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h3>Paragrafo originale</h3>
+    <p><?=$text_area ?></p>
+    <p><i>il paragrafo era lungo <b><?=$text_length ?></b> caratteri</i></p>
+    <hr>
+    <h3>Paragrafo Censurato</h3>
+    <p><?=$censored_word?></p>
+    <p><i>il paragrafo era lungo <b><?=$text_length ?></b> caratteri</i></p>
 </body>
 </html>
